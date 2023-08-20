a5ba2c6a5e89cdb05738adb2ccd1862240781e14
hugo <hugo@hugo.com>
2023-08-20 19:48:43 +0700

    sdds

--- todo-app-vue/src/components/TodoList.vue
+++ todo-app-vue/src/components/TodoList.vue
@@ -5,38 +5,9 @@
         <span class="delete" @click="emit('deleteItem', index)">x</span>
       </li>
     </ul>
-    <!-- {{ props.todoList }} -->
-    <!-- {{ props.todoList }} -->
-    <!-- <ul>
-      
-      <li      
-        v-for="(todo, index) in TodoList" :key="index">
-        <span class="name">{{todo.name}}</span>
-        <span class="delete" @click="Delete(todo.id)">x</span>
-      </li>
-    </ul> -->
   </template>
 
 <script setup>
   const emit = defineEmits(['deleteItem', 'submit']);
   const props = defineProps(['todoList']);
-
-  console.log(props);
 </script>
-  
-  <!-- <script>
-  import { defineEmits, defineProps } from 'vue';
-  
-  export default {
-    setup() {
-      const emit = defineEmits(['deleteItem', 'submit']);
-      const props = defineProps(['todoList']);      
-      console.log(props);
-      return {
-        emit,
-        props,
-      };
-    },
-  };
-  </script> -->
-  
