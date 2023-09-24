f3312fa9a87b4881ed54d9ba24e73012e1772497
hugo <hugo@hugo.com>
2023-09-24 20:48:22 +0700

    6446556

--- da-pak-is-2-test-6-constra-3/web/src/main.js
+++ da-pak-is-2-test-6-constra-3/web/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import HomeComponent from "./components/HomeComponent.vue"
 import ProjectsComponent from "./components/ProjectsComponent.vue"
 import ProjectDetailComponent from "./components/ProjectDetailComponent.vue"
@@ -10,9 +9,9 @@ import { createRouter, createWebHistory } from "vue-router"
 const app = createApp(App)
 
 const routes = [
-    { path: "/project/:id", component: ProjectDetailComponent },
+    { path: "/", component: HomeComponent },
     { path: "/projects", component: ProjectsComponent },
-	{ path: "/", component: HomeComponent }
+    { path: "/project/:id", component: ProjectDetailComponent },
 ]
 
 const router = createRouter({
@@ -20,9 +19,8 @@ const router = createRouter({
     routes
 })
 
-app.config.globalProperties.$apiURL = "http://127.0.0.1:8000/api"
 app.config.globalProperties.$apiBaseURL = "http://127.0.0.1:8000"
+app.config.globalProperties.$apiURL = "http://127.0.0.1:8000/api"
 
 app.use(router)
-
 app.mount('#app')
--- da-pak-is-2-test-7-append/src/App.vue
+++ da-pak-is-2-test-7-append/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <app-header />
+    <router-view />
+  <app-footer />
 </template>
 
 <script>
