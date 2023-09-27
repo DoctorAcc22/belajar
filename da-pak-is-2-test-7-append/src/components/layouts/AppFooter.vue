fe9b2d8ecce8a286a8399902ca754152b6ab94c6
hugo <hugo@hugo.com>
2023-09-27 07:40:38 +0700

    asdas

---
+++ da-pak-is-2-test-7-append/src/components/layouts/AppFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- copas footer dari template kesini -->
+    <p>This is footer</p>
+</template>
+
+<script>
+	// contoh import file js
+	// import "../../assets/plugins/bootstrap/bootstrap.min.js"
+	// import "../../assets/plugins/slick/slick.min.js"
+	// import "../../assets/plugins/slick/slick-animation.min.js"
+	// import "../../assets/plugins/colorbox/jquery.colorbox.js"
+	// import "../../assets/js/script.js"
+	
+	export default {
+		name: "AppFooter",
+
+		data () {
+			return {
+				year: new Date().getFullYear()
+			}
+		}
+	}
+</script>
---
+++ da-pak-is-2-test-7-append/src/components/layouts/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <title>Home - Append Bootstrap Template</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+        <link href="../../assets/img/favicon.png" rel="icon">
+        <link href="../../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <link href="https://fonts.googleapis.com" rel="preconnect">
+        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    </head>
+
+    <div style="apple-touch-icon"></div>
+</template>
+
+<script>
+    // contoh import file css
+    // import "../../assets/plugins/bootstrap/bootstrap.min.css"
+    // import "../../assets/plugins/fontawesome/css/all.min.css"
+    // import "../../assets/plugins/animate-css/animate.css"
+    // import "../../assets/plugins/slick/slick.css"
+    // import "../../assets/plugins/slick/slick-theme.css"
+    // import "../../assets/plugins/colorbox/colorbox.css"
+    // import "../../assets/css/style.css"
+
+    // import "../../assets/vendor/bootstrap/css/bootstrap.min.css"
+    // import "../../assets/vendor/bootstrap-icons/bootstrap-icons.css"
+    // import "../../assets/vendor/glightbox/css/glightbox.min.css"
+    // import "../../assets/vendor/swiper/swiper-bundle.min.css"
+    // import "../../assets/vendor/aos/aos.css"
+    // import "../../assets/css/main.css"
+
+	export default {
+		name: "AppHeader"
+	}
+</script>
