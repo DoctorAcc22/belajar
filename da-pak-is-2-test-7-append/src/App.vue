e557204b6e6014de962797113754636333679747
hugo <hugo@hugo.com>
2023-09-27 07:40:23 +0700

    asdasd

--- da-pak-is-2-test-7-append/src/App.vue
+++ da-pak-is-2-test-7-append/src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <app-header />
-    <router-view />
-  <app-footer />
+	<app-header />
+		<router-view />
+	<app-footer />
+    <div style="apple-touch-icon"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// pastikan sudah ada file yg mau di import
+import AppHeader from "./components/layouts/AppHeader.vue"
+import AppFooter from "./components/layouts/AppFooter.vue"
 
 export default {
   name: 'App',
+  
+  mounted() {
+    // // Tambahkan tautan favicon
+    // const faviconLink = document.createElement('link');
+    // faviconLink.href = require('../src/assets/img/favicon.png'); // Pastikan path-nya benar
+    // faviconLink.rel = 'icon';
+    // document.head.appendChild(faviconLink);
+
+    // // Tambahkan tautan ikon sentuh
+    // const appleTouchIconLink = document.createElement('link');
+    // // appleTouchIconLink.href = require('../src/assets/img/apple-touch-icon.png'); // Pastikan path-nya benar
+    // appleTouchIconLink.href = require('../src/assets/img/'); // Pastikan path-nya benar
+    // appleTouchIconLink.rel = 'apple-touch-icon';
+    // document.head.appendChild(appleTouchIconLink);
+
+
+  },
+
   components: {
-    HelloWorld
+    AppHeader,
+    AppFooter
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- da-pak-is-2-test-7-append/src/main.js
+++ da-pak-is-2-test-7-append/src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import HomeComponent from "./components/HomeComponent.vue"
+// import ProjectsComponent from "./components/ProjectsComponent.vue"
+// import ProjectDetailComponent from "./components/ProjectDetailComponent.vue"
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from "vue-router"
+
+const app = createApp(App)
+
+const routes = [
+    // { path: "/", component: HomeComponent }, //Home
+    // { path: "/projects", component: ProjectsComponent }, //Halaman Projects
+    // { path: "/project/:id", component: ProjectDetailComponent }, //Project by id
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+app.config.globalProperties.$baseURL = "http://127.0.0.1:8000"
+app.config.globalProperties.$apiURL = "http://127.0.0.1:8000/api"
+
+app.use(router)
+app.mount('#app')
