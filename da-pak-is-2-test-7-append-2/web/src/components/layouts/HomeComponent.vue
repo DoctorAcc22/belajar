a1658697b645ffa7829cf2b49e528a1f84835a72
hugo <hugo@hugo.com>
2023-09-30 16:58:30 +0700

    64564

---
+++ da-pak-is-2-test-7-append-2/web/src/components/layouts/HomeComponent.vue
@@ -0,0 +1,68 @@
+<template>
+    <HeroSection />
+
+    <ClientSection />
+
+    <AboutSection />
+
+    <StatsSection />
+
+    <ServicesSection />
+
+    <FeaturesSection />
+
+    <PortfolioSection />
+
+    <PricingSection />
+
+    <FaqSection />
+
+    <TeamSection />
+
+    <CallToActionSection />
+
+    <TestimonialsSection />    
+
+    <RecentPostsSection />
+
+    <ContactSection />
+
+</template>
+
+<script>
+    import HeroSection from "./HeroSection.vue";
+    import ClientSection from "./ClientSection.vue";
+    import AboutSection from "./AboutSection.vue";
+    import StatsSection from "./StatsSection.vue";
+    import ServicesSection from "./ServicesSection.vue";
+    import FeaturesSection from "./FeaturesSection.vue";
+    import PortfolioSection from "./PortfolioSection.vue";
+    import PricingSection from "./PricingSection.vue";
+    import FaqSection from "./FaqSection.vue";
+    import TeamSection from "./TeamSection.vue";
+    import CallToActionSection from "./CallToActionSection.vue";
+    import TestimonialsSection from "./TestimonialsSection.vue";
+    import RecentPostsSection from "./RecentPostsSection.vue";
+    import ContactSection from "./ContactSection.vue";
+
+	export default {
+		name: "HomeComponent",
+        
+        components: {
+            HeroSection,
+            ClientSection,
+            AboutSection,
+            StatsSection,
+            ServicesSection,
+            FeaturesSection,
+            PortfolioSection,
+            PricingSection,
+            FaqSection,
+            TeamSection,
+            CallToActionSection,
+            TestimonialsSection,
+            RecentPostsSection,
+            ContactSection,
+        }
+	}
+</script>
