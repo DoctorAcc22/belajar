4b169a2b64aa14d1825d57391a52fd7c6b1678c0
hugo <hugo@hugo.com>
2023-10-08 21:02:07 +0700

    465565

---
+++ da-pak-is-2-test-7-append-3/web/src/components/layouts/WhyUsSection.vue
@@ -0,0 +1,74 @@
+<template>
+    <section id="services" class="services">
+
+		<div class="container section-title" data-aos="fade-up">
+			<h2>Kenapa Memilih Doctor Accounting?</h2>
+			<p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+		</div>
+
+		<div class="container">
+
+			<div class="row gy-4">
+
+				<div class="col-lg-12 " data-aos="fade-up" data-aos-delay="100">
+					<div class="service-item d-flex">
+						<div class="icon flex-shrink-0"><i class="bi bi-briefcase"></i></div>
+						<div>
+							<h4 class="title"><a href="services-details.html" class="stretched-link">PROFESSIONAL SERVICES</a></h4>
+							<p class="description">Staff Tenaga ahli berpengalaman dengan jam terbang yang tinggi, kami pastikan service kepada Anda selalu dilakukan secara professional.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-12 " data-aos="fade-up" data-aos-delay="200">
+					<div class="service-item d-flex">
+					<div class="icon flex-shrink-0"><i class="bi bi-card-checklist"></i></div>
+					<div>
+						<h4 class="title"><a href="services-details.html" class="stretched-link">FASTEST RESPONSE</a></h4>
+						<p class="description">Tersedianya tim service dan teknis memastikan Anda mendapatkan respons yang cepat untuk solusi kebutuhan IT Anda.</p>
+					</div>
+					</div>
+				</div>
+
+				<div class="col-lg-12 " data-aos="fade-up" data-aos-delay="300">
+					<div class="service-item d-flex">
+					<div class="icon flex-shrink-0"><i class="bi bi-bar-chart"></i></div>
+					<div>
+						<h4 class="title"><a href="services-details.html" class="stretched-link">GUARANTEED PRODUCT</a></h4>
+						<p class="description">Semua product yang kami distribusikan bergaransi resmi global dan bukan barang loose-pack ataupun pre-owned.</p>
+					</div>
+					</div>
+				</div>
+
+				<div class="col-lg-12 " data-aos="fade-up" data-aos-delay="400">
+					<div class="service-item d-flex">
+					<div class="icon flex-shrink-0"><i class="bi bi-binoculars"></i></div>
+					<div>
+						<h4 class="title"><a href="services-details.html" class="stretched-link">ON-SITE SERVICE SUPPORT</a></h4>
+						<p class="description">Kami menyediakan on site service support langsung di tempat Anda, untuk memastikan downtime yg Anda alami selalu minimal.</p>
+					</div>
+					</div>
+				</div>
+
+				<div class="col-lg-12 " data-aos="fade-up" data-aos-delay="500">
+					<div class="service-item d-flex">
+					<div class="icon flex-shrink-0"><i class="bi bi-brightness-high"></i></div>
+					<div>
+						<h4 class="title"><a href="services-details.html" class="stretched-link">ALL-AROUND SOLUTION</a></h4>
+						<p class="description">Solusi hardware, solusi software, pekerjaan lapangan, network, serta cabling. Tenaga teknis terpadu kami siap memenuhi semua kebutuhan di tempat/perusahaan yang Anda kelola.</p>
+					</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+</template>
+
+<script>
+	export default {
+		name: "WhyUs"
+	}
+</script>
---
+++ da-pak-is-2-test-7-append-3/web/src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import HomeComponent from "./components/layouts/HomeComponent.vue"
+// import ProjectsComponent from "./components/ProjectsComponent.vue"
+// import ProjectDetailComponent from "./components/ProjectDetailComponent.vue"
+import { createRouter, createWebHistory } from "vue-router"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+const app = createApp(App)
+
+app.config.globalProperties.$baseURL = "http://127.0.0.1:8000"
+app.config.globalProperties.$apiURL = "http://127.0.0.1:8000/api"
+
+const routes = [
+    { path: "/", component: HomeComponent }, //Home
+    // { path: "/projects", component: ProjectsComponent }, //Halaman Projects
+    // { path: "/project/:id", component: ProjectDetailComponent }, //Project by id
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+app.AOS = new AOS.init()
+
+app.use(AOS)
+app.use(router)
+app.mount('#app')
