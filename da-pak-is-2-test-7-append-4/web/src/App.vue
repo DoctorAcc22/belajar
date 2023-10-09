8a21840d4355645b24e01fdb3f50d09f7adfe701
hugo <hugo@hugo.com>
2023-10-09 11:54:23 +0700

    64655

---
+++ da-pak-is-2-test-7-append-4/web/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<AppHeader />
+    <main id="main">
+      <router-view />
+    </main>
+	<AppFooter />
+</template>
+
+<script>
+// pastikan sudah ada file yg mau di import
+import AppHeader from "./components/layouts/AppHeader.vue"
+import AppFooter from "./components/layouts/AppFooter.vue"
+
+export default {
+  name: 'App',
+  
+  mounted() {
+    const faviconLink = document.createElement('link');
+    faviconLink.href = require('../src/assets/img/favicon.png'); // Pastikan path-nya benar
+    faviconLink.rel = 'icon';
+    document.head.appendChild(faviconLink);
+
+    const appleTouchIconLink = document.createElement('link');
+    appleTouchIconLink.href = require('../src/assets/img/apple-touch-icon.png'); // Pastikan path-nya benar    
+    appleTouchIconLink.rel = 'apple-touch-icon';
+    document.head.appendChild(appleTouchIconLink);
+  },
+
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
