596a1517de2bcc1aceb4798137f01cffc6c14a64
DoctorAcc22 <105140626+DoctorAcc22@users.noreply.github.com>
2023-10-15 04:34:40 +0700

    5644

--- test-codebase-3/src/router/index.js
+++ test-codebase-3/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
+import LoginView from '../components/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
