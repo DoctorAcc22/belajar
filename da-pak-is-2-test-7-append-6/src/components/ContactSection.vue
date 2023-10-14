bcb64405749714aa74942a33ebb1c4fb1dc20930
DoctorAcc22 <105140626+DoctorAcc22@users.noreply.github.com>
2023-10-15 05:17:30 +0700

    4564

---
+++ da-pak-is-2-test-7-append-6/src/components/ContactSection.vue
@@ -0,0 +1,77 @@
+<template>
+    <section id="contact" class="contact">
+
+		<div class="container section-title" data-aos="fade-up">
+			<h2>Contact</h2>
+			<p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+		</div>
+
+		<div class="container" data-aos="fade-up" data-aos-delay="100">
+
+			<div class="row gy-4">
+
+				<div class="col-lg">
+
+					<div class="row gy-4">
+						<div class="col-md-6">
+							<div class="info-item" data-aos="fade" data-aos-delay="200">
+								<i class="bi bi-geo-alt"></i>
+								<h3>Address</h3>
+								<p>Jl. Kolonel Sugiri No. 22</p>
+								<p>Purwokerto - Jawa Tengah</p>
+								<p>Indonesia</p>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="info-item" data-aos="fade" data-aos-delay="300">
+								<i class="bi bi-whatsapp"></i>
+								<h3>Call Us (WhatsApp)</h3>
+
+								<p>
+									<a href="https://wa.me/6285227487277?text=hi" target="blank_">0852 2748 7277</a> Beda Ganesha (Yogyakarta)
+								</p>
+								<p>
+									<a href="https://wa.me/6282227637711?text=hi" target="blank_">0822 2763 7711</a> Hugo (Purwokerto)
+								</p>
+								<p>
+									<a href="https://wa.me/6281215322206?text=hi" target="blank_">0812 1532 2206</a> Denish (Purwokerto)
+								</p>																								
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="info-item" data-aos="fade" data-aos-delay="400">
+								<i class="bi bi-envelope"></i>
+								<h3>Email Us</h3>
+								<p>doctoracc22@gmail.com</p>
+								<p></p>
+								<p></p>
+								<p></p>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="info-item" data-aos="fade" data-aos-delay="500">
+								<i class="bi bi-clock"></i>
+								<h3>Open Hours</h3>
+								<p>Monday - Saturday 9:00AM - 05:00PM</p>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+</template>
+
+<script>
+	export default {
+		name: "ContactSection",
+	}	
+</script>
---
+++ da-pak-is-2-test-7-append-6/src/components/HeroSection.vue
@@ -0,0 +1,70 @@
+<template>
+	<section id="hero" class="hero">
+
+		<img src="/img/hero-bg.jpg" class="bg" alt="" data-aos="fade-in">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg">
+					<div class="row">
+						<img src="/img/logo-transparent.png" class="logo" alt=""
+							data-aos="fade-in">
+					</div>
+					<div class="row">
+						<h1 data-aos="fade-up" data-aos-delay="100">PROGRAM PERUSAHAAN (SCM, ERP, CRM)</h1>
+						<h1 data-aos="fade-up" data-aos-delay="100">PROGRAM KASIR (POS)</h1>
+					</div>
+					<div class="row">
+						<p class="about-us" data-aos="fade-up" data-aos-delay="200">
+							<b>About Us :</b>
+						</p>
+					</div>
+					<div class="row">
+						<p data-aos="fade-up" data-aos-delay="200">
+							Doctor Computer yang lebih dikenal dengan product Doctor Accounting, didirikan pada tahun 2013.
+						</p>
+						<p data-aos="fade-up" data-aos-delay="200">
+							Fokus utama product dan service kami adalah pada bidang software akuntansi.
+						</p>
+						<p data-aos="fade-up" data-aos-delay="200">
+							Kami juga menyediakan segala kebutuhan komputer dan perlengkapan networking dengan kualitas
+							unggul.
+						</p>
+						<p data-aos="fade-up" data-aos-delay="200">
+							Selalu ada garansi after sales support service untuk seluruh pelanggan kami.
+						</p>
+					</div>
+					<!-- <div class="row">
+						<div class="col-lg">
+							<form action="#" class="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
+								<input type="text" class="form-control" placeholder="Enter email address">
+								<input type="submit" class="btn btn-primary" value="Sign up">
+							</form>
+						</div>
+					</div> -->
+
+				</div>
+
+				<div class="row">
+					<div class="col-lg">
+						<!-- <input class="btn btn-primary std-button" value="Download" data-aos="fade-up" data-aos-delay="300"> -->
+						<!-- <a href="#" class="read-more"><span>Read More</span><i class="bi bi-arrow-right"></i></a> -->
+						<a href="https://drive.google.com/file/d/1T4cEcdvYahr4fSLgtUmXu_6Bk7jYjg6V/view?usp=sharing"
+							class="btn btn-primary std-button" target="blank_">Download Demo</a>
+					</div>
+				</div>
+
+				<!-- <div class="col-lg-5">
+					<img src="/img/accountant.png" class="accountant" alt="" data-aos="fade-in">
+				</div> -->
+			</div>
+
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "HeroSection"
+}
+</script>
